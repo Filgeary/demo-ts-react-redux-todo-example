ef2e38ff82b306d85b3abce3db6091c2e050d543
Georgiy Filippov <g.filippov777@gmail.com>
2021-11-28 09:59:33 +0500

    add state & counter handlers

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,13 +7,44 @@ interface AppProps {
 }
 
 class App extends React.Component<AppProps> {
+  state = {
+    counter: 0,
+  }
+
+  handleIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 })
+  }
+
+  handleDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 })
+  }
+
   render() {
     const { title, info } = this.props
+    const { counter } = this.state
 
     return (
-      <div>
+      <div className={'container-lg'}>
         <h1>{title}</h1>
         <p>{info?.toLowerCase()}</p>
+
+        <div className={'btn-group-lg'}>
+          <button
+            type={'button'}
+            className={'btn btn-lg btn-primary'}
+            onClick={this.handleIncrement}
+          >
+            <i className={'bi-plus-circle'} />
+          </button>
+          <button
+            type={'button'}
+            className={'btn btn-lg btn-secondary'}
+            onClick={this.handleDecrement}
+          >
+            <i className={'bi-dash-circle'} />
+          </button>
+          <h2>{counter}</h2>
+        </div>
       </div>
     )
   }
